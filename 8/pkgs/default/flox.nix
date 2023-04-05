50b4d351c2584132e7788c00f0383d7230dc8d33
Matthew Kenigsberg <matthewkenigsberg@gmail.com>
2023-04-05 11:35:21 -0600

    matthew installed stable.nixpkgs-flox.hello
    flox install hello
      stable.nixpkgs-flox.hello: ∅ -> 2.12.1

---
+++ 8/pkgs/default/flox.nix
@@ -0,0 +1,30 @@
+{...}: {
+  packages = {
+    nixpkgs-flox = {
+      # hello = {version = "2.12.1";};
+    };
+  };
+  packages.nixpkgs-flox.openssh = {};
+  packages.nixpkgs-flox.hello = {};
+  packages.nixpkgs-flox.openssh = {};
+  packages.nixpkgs-flox.python = {};
+  packages.nixpkgs-flox.neovim = {};
+  packages.nixpkgs-flox.neovim = {};
+  packages.nixpkgs-flox.curl = {};
+  packages.nixpkgs-flox.jq = {};
+  packages.nixpkgs-flox.jq = {};
+  packages.nixpkgs-flox.ripgrep = {};
+  packages.nixpkgs-flox.ripgrep = {};
+  shell = {
+    aliases = {
+      # gg = "git grep";
+    };
+    # POSIX shell hook, except curly braces have to be escaped like ``${
+    hook = ''
+      # echo "Supercharged by flox!" 1>&2
+    '';
+  };
+  environmentVariables = {
+    # LANG = "en_US.UTF-8";
+  };
+}
