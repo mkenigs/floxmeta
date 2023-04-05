9fb10fed49f71f22bd4c13affe5adcde3a71f192
Matthew Kenigsberg <matthewkenigsberg@gmail.com>
2023-04-05 11:13:28 -0600

    matthew installed stable.nixpkgs-flox.openssh stable.nixpkgs-flox.ripgrep stable.nixpkgs-flox.jq
    flox install openssh ripgrep jq
      stable.nixpkgs-flox.jq: ∅ -> 1.6
      stable.nixpkgs-flox.openssh: ∅ -> 9.2p1
      stable.nixpkgs-flox.ripgrep: ∅ -> 13.0.0

---
+++ 1/pkgs/default/flox.nix
@@ -0,0 +1,22 @@
+{...}: {
+  packages = {
+    nixpkgs-flox = {
+      # hello = {version = "2.12.1";};
+    };
+  };
+  packages.nixpkgs-flox.openssh = {};
+  packages.nixpkgs-flox.jq = {};
+  packages.nixpkgs-flox.ripgrep = {};
+  shell = {
+    aliases = {
+      # gg = "git grep";
+    };
+    # POSIX shell hook, except curly braces have to be escaped like ``${
+    hook = ''
+      # echo "Supercharged by flox!" 1>&2
+    '';
+  };
+  environmentVariables = {
+    # LANG = "en_US.UTF-8";
+  };
+}
