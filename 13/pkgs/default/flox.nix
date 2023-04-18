9529eb0b13ae8522552fdac184b524d267e80177
Matthew Kenigsberg <matthewkenigsberg@gmail.com>
2023-04-18 10:39:44 -0600

    matthew installed stable.nixpkgs-flox.qemu
    flox install qemu
      stable.nixpkgs-flox.qemu: ∅ -> 7.2.0

---
+++ 13/pkgs/default/flox.nix
@@ -0,0 +1,34 @@
+{...}: {
+  packages = {
+    nixpkgs-flox = {
+      # hello = {version = "2.12.1";};
+    };
+  };
+  packages.flox.flox-prerelease = {};
+  packages.nixpkgs-flox.openssh = {};
+  packages.nixpkgs-flox.qemu = {};
+  packages.nixpkgs-flox.podman = {};
+  packages.nixpkgs-flox.alejandra = {};
+  packages.nixpkgs-flox.hello = {};
+  packages.nixpkgs-flox.openssh = {};
+  packages.nixpkgs-flox.python = {};
+  packages.nixpkgs-flox.neovim = {};
+  packages.nixpkgs-flox.neovim = {};
+  packages.nixpkgs-flox.curl = {};
+  packages.nixpkgs-flox.jq = {};
+  packages.nixpkgs-flox.jq = {};
+  packages.nixpkgs-flox.ripgrep = {};
+  packages.nixpkgs-flox.ripgrep = {};
+  shell = {
+    aliases = {
+      # gg = "git grep";
+    };
+    # POSIX shell hook, except curly braces have to be escaped like ``${
+    hook = ''
+      # echo "Supercharged by flox!" 1>&2
+    '';
+  };
+  environmentVariables = {
+    # LANG = "en_US.UTF-8";
+  };
+}
